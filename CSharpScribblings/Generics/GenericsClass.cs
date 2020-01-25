89ee240652118fffd163f26ce4d295d575e007ef
Chris Clark <197.chris@gmail.com>
2020-01-25 16:33:19 +0000

    Create generics class
    Add generics class which can type an array of type T and then display said values

---
+++ CSharpScribblings/Generics/GenericsClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class GenericsClass<T>
+    {
+        // generic array instance variable
+        private T[] vals;
+
+        public GenericsClass(T[] input)
+        {
+            vals = input;
+        }
+
+        public string DisplayValues()
+        {
+            string str = null;
+
+            foreach (T t in vals)
+            {
+                str += $"Value={t} "; 
+            }
+
+            return str;
+        }
+    }
+}
---
+++ CSharpScribblings/Generics/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericsClass<int> ints = new GenericsClass<int>(new int[] { 1, 2, 3, 4, 5 });
+            Console.WriteLine(ints.DisplayValues());
+
+            GenericsClass<double> dubs = new GenericsClass<double>(new double[] { 1.0, -2.3, 3, 4, 5 });
+            Console.WriteLine(dubs.DisplayValues());
+
+            GenericsClass<decimal> decs = new GenericsClass<decimal>(new decimal[] { 1, 2.0M, 3, 4, 5.79M });
+            Console.WriteLine(decs.DisplayValues());
+        }
+    }
+}

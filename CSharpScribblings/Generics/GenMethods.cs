81b61a1c15212e89931e8a4fe070c8e1a3591e14
Chris Clark <197.chris@gmail.com>
2020-02-16 19:53:24 +0000

    Add new GenMethods class to swap/compare values

---
+++ CSharpScribblings/Generics/GenMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Generics
+{
+    public class GenMethods
+    {
+        // Method can operate on multiple data types
+        public static void Swap<T>(ref T x, ref T y)
+        {
+            T temp = x;
+            // Save x
+            x = y;
+            // Assign y to x
+            y = temp;
+            // Assign original value to x back to y
+        }
+
+        // The function has a contraint, so it can operate on values
+        // that can be compared
+        public static string Compare<T>(T x, T y) where T : IComparable
+        {
+            // CompareTo retrun < 0, =0, or > 0, depending on the relationship
+            // between x and y
+            if (x.CompareTo(y) < 0)
+            {
+                return $"{x}<{y}";
+            }
+            else
+            {
+                return $"{x}>{y}";
+            }
+        } 
+    }
+}
--- CSharpScribblings/Generics/Program.cs
+++ CSharpScribblings/Generics/Program.cs
@@ -14,6 +14,17 @@ namespace Generics
 
             GenericsClass<decimal> decs = new GenericsClass<decimal>(new decimal[] { 1, 2.0M, 3, 4, 5.79M });
             Console.WriteLine(decs.DisplayValues());
+
+            double x = 25, y = 34;
+            GenMethods.Swap<double>(ref x, ref y);
+
+            Console.WriteLine(GenMethods.Compare<double>(x, y));
+
+            int x2 = 25, y2 = 34;
+            GenMethods.Swap<int>(ref x2, ref y2);
+
+            Console.WriteLine(GenMethods.Compare<int>(x2, y2));
+
         }
     }
 }
